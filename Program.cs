94a399ac72311fe15832834f9dcab57e6e476d81
Richard Carrigan <nihonjinboy85@gmail.com>
2021-04-10 22:44:26 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ctci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Permutation("Richard");
+        }
+
+        static void Permutation(string str)
+        {
+            Permutation(str, "");
+        }
+        static void Permutation(string str, string prefix)
+        {
+            if(str.Length == 0)
+            {
+                Console.WriteLine(prefix);
+            }
+            else
+            {
+                for(int i = 0; i < str.Length; i++)
+                {
+                    string rem = str.Substring(0, i) + str.Substring(i + 1);
+                    Permutation(rem, prefix + str[i]);
+                }
+            }
+        }
+    }
+}
